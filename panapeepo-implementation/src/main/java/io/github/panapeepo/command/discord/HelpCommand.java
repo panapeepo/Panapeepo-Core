a6ee9212868e2c0e97da6ee13c9a7cc848bd1ea4
rob <robertschuck1111@gmail.com>
2020-07-22 02:57:58 +0200

    Send the correct guild prefix in the help command.

--- panapeepo-implementation/src/main/java/io/github/panapeepo/command/discord/HelpCommand.java
+++ panapeepo-implementation/src/main/java/io/github/panapeepo/command/discord/HelpCommand.java
@@ -4,6 +4,7 @@ import com.github.derrop.simplecommand.annotation.Command;
 import com.github.derrop.simplecommand.annotation.SubCommand;
 import io.github.panapeepo.api.Panapeepo;
 import io.github.panapeepo.api.command.DiscordCommandSender;
+import io.github.panapeepo.api.provider.PanapeepoGuildProvider;
 
 import javax.annotation.Nonnull;
 
@@ -22,10 +23,16 @@ public class HelpCommand {
     public void handle(@Nonnull DiscordCommandSender sender) {
         var user = sender.getMember().getUser();
         this.panapeepo.sendDefaultEmbed(sender.getChannel(), user, embed -> {
+
+            var prefix = this.panapeepo.getServiceRegistry().getProvider(PanapeepoGuildProvider.class).map(guildProvider -> {
+                var guild = guildProvider.getGuild(sender.getMember().getGuild().getIdLong());
+                return guild == null ? null : guild.getPrefix();
+            }).orElse(this.panapeepo.getConfig().getCommandPrefix());
+
             embed.setTitle("Help");
             embed.setThumbnail(sender.getChannel().getJDA().getSelfUser().getAvatarUrl());
             embed.setDescription(
-                    String.format("The current Command-Prefix for this Guild is `%s`.", this.panapeepo.getConfig().getCommandPrefix())
+                    String.format("The current Command-Prefix for this Guild is `%s`.", prefix)
             );
 
             panapeepo.getDiscordCommandMap().getCommands().forEach(command -> {
