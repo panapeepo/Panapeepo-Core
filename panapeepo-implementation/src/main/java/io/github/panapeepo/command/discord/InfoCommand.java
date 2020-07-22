5221623ab28534dc557429b9049f8f8fc289aa76
rob <robertschuck1111@gmail.com>
2020-07-22 03:00:31 +0200

    Revert "Added "Database" to the Info Command"

--- panapeepo-implementation/src/main/java/io/github/panapeepo/command/discord/InfoCommand.java
+++ panapeepo-implementation/src/main/java/io/github/panapeepo/command/discord/InfoCommand.java
@@ -4,7 +4,6 @@ import com.github.derrop.simplecommand.annotation.Command;
 import com.github.derrop.simplecommand.annotation.SubCommand;
 import io.github.panapeepo.api.Panapeepo;
 import io.github.panapeepo.api.command.DiscordCommandSender;
-import io.github.panapeepo.api.database.DatabaseDriver;
 
 import javax.annotation.Nonnull;
 
@@ -26,19 +25,13 @@ public class InfoCommand {
             embed.setTitle("Info");
             embed.setThumbnail(sender.getChannel().getJDA().getSelfUser().getAvatarUrl());
 
-            embed.addField("Version (Commit)", String.format("%s (%s)", this.panapeepo.getCurrentVersion(), this.panapeepo.getCurrentCommit()), true);
+            embed.addField("Version", String.format("%s (Commit: %s)", this.panapeepo.getCurrentVersion(), this.panapeepo.getCurrentCommit()), true);
             embed.addField("Memory usage", String.format("%.2f MB / %.2f MB", panapeepo.getUsedMemory(), panapeepo.getMaxMemory()), true);
             embed.addField("Sourcecode", String.format("[Github](%s)", "https://github.com/Panapeepo/Panapeepo-Core"), true);
-            embed.addField("Shards (Current Shard)", String.format("%d / %d (#%d)", this.panapeepo.getShardManager().getShardsRunning(), this.panapeepo.getShardManager().getShardsTotal(), user.getJDA().getShardInfo().getShardId() + 1), true);
+            embed.addField("Shards", String.format("%d / %d (current #%d)", this.panapeepo.getShardManager().getShardsRunning(), this.panapeepo.getShardManager().getShardsTotal(), user.getJDA().getShardInfo().getShardId() + 1), true);
             embed.addField("Java Version", System.getProperty("java.version"), true);
             embed.addField("Operating System", System.getProperty("os.name") + " - " + System.getProperty("os.arch"), true);
             embed.addField("Uptime", this.panapeepo.formatMillis(System.currentTimeMillis() - this.panapeepo.getStartupTime()), true);
-
-            this.panapeepo.getServiceRegistry().getProvider(DatabaseDriver.class).ifPresent(databaseDriver -> {
-                embed.addField("Database (Tables)", String.format("%s (%d)",
-                        databaseDriver.getClass().getSimpleName(),
-                        databaseDriver.getTableNames().size()), true);
-            });
         });
     }
 
